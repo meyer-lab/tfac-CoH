d8d004e2ce34d2fe58d3cdf2103c5f8434d9c717
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2022-05-24 12:15:05 -0700

    More

---
+++ CoH/figures/figure1.py
@@ -0,0 +1,14 @@
+"""
+This creates Figure 1.
+"""
+from .figureCommon import subplotLabel, getSetup
+
+def makeFigure():
+    """Get a list of the axis objects and create a figure."""
+    # Get list of axis objects
+    ax, f = getSetup((8, 4), (1, 1))
+
+    # Add subplot labels
+    subplotLabel(ax)
+
+    return f
