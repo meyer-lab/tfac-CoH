9cefe297c2b3c7de139dbbe22af46bd831675be7
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2022-12-22 14:49:46 -0800

    Leaving off

--- coh/figures/figure3.py
+++ coh/figures/figure3.py
@@ -18,7 +18,7 @@ path_here = dirname(dirname(__file__))
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
     # Get list of axis objects
-    ax, f = getSetup((8, 6), (3, 2))
+    ax, f = getSetup((8, 6), (3, 2), multz={0: 1})
 
     # Add subplot labels
     subplotLabel(ax)
@@ -29,16 +29,31 @@ def makeFigure():
     CoH_Data = xa.open_dataarray(join(path_here, "data/CoHTensorDataJustSignal.nc"))
     tFacAllM, _ = factorTensor(CoH_Data.values, numComps=num_comps)
     cp_normalize(tFacAllM)
-    make_alldata_DF(CoH_Data, PCA=False)
-    CoH_LogReg_plot(ax[1], tFacAllM, CoH_Data, num_comps)
+    #make_alldata_DF(CoH_Data, PCA=False)
+   
+    
     matrix_DF = pd.read_csv(join(path_here, "data/CoH_Matrix.csv"), index_col=0).dropna(axis='columns').set_index("Patient")
 
-    BC_status_plot(10, CoH_Data, matrix_DF, ax[0], abund=False)
+    #BC_status_plot(15, CoH_Data, matrix_DF, ax[1], abund=False)
+    #CoH_LogReg_plot(ax[2], tFacAllM, CoH_Data, num_comps)
 
-    plot_tFac_CoH(ax[2], tFacAllM, CoH_Data, "Treatment", numComps=num_comps)
-    plot_tFac_CoH(ax[3], tFacAllM, CoH_Data, "Cell", numComps=num_comps)
-    CoH_DF = pd.read_csv(join(path_here, "data/CoH_Flow_DF.csv"))
-    BC_scatter(ax[4], CoH_DF, "pSTAT3", "IL10-50ng")
-    BC_scatter(ax[5], CoH_DF, "pSTAT5", "IL2-50ng", cells=["Treg", "CD4+", "CD8+"])
+    CoH_Bar_Plot(ax[3], tFacAllM, CoH_Data, "Treatment", numComps=12, plot_comps=[8, 11])
+    CoH_Bar_Plot(ax[4], tFacAllM, CoH_Data, "Cell", numComps=12, plot_comps=[8, 11])
+    #CoH_DF = pd.read_csv(join(path_here, "data/CoH_Flow_DF.csv"))
+    #BC_scatter(ax[5], CoH_DF, "pSTAT3", "IL10-50ng")
+    #BC_scatter(ax[6], CoH_DF, "pSTAT5", "IL2-50ng", cells=["Treg", "CD4+", "CD8+"])
 
     return f
+
+
+def CoH_Bar_Plot(ax, tFac, CoH_Array, mode, numComps, plot_comps):
+    """Plots bar plot for spec"""
+    mode_labels = CoH_Array[mode]
+    coord = CoH_Array.dims.index(mode)
+    mode_facs = tFac[1][coord]
+    tFacDF = pd.DataFrame()
+
+    for i in range(0, numComps):
+        tFacDF = pd.concat([tFacDF, pd.DataFrame({"Component_Val": mode_facs[:, i], "Component": (i + 1), mode: mode_labels})])
+    tFacDF = tFacDF.loc[tFacDF["Component"].isin(plot_comps)]
+    sns.barplot(data=tFacDF, x=mode, y="Component_Val", hue="Component", ax=ax)
---
+++ coh/figures/figure3hm.py
@@ -0,0 +1,53 @@
+"""
+This creates Figure 3's clustered heat map.
+"""
+import xarray as xa
+import pandas as pd
+import seaborn as sns
+from tensorpack.cmtf import cp_normalize
+from .figureCommon import subplotLabel, getSetup
+from os.path import join, dirname
+from ..tensor import factorTensor
+
+
+path_here = dirname(dirname(__file__))
+
+
+def makeFigure():
+    """Get a list of the axis objects and create a figure."""
+    # Get list of axis objects
+    ax, f = getSetup((6, 3), (1, 1))
+
+    # Add subplot labels
+    #subplotLabel(ax)
+
+    num_comps = 12
+    CoH_Data = xa.open_dataarray(join(path_here, "data/CoHTensorDataJustSignal.nc"))
+    tFacAllM, _ = factorTensor(CoH_Data.values, numComps=num_comps)
+    cp_normalize(tFacAllM)
+    f = plot_coh_clust(tFacAllM, CoH_Data, "Patient", numComps=num_comps)
+
+
+    return f
+
+
+def plot_coh_clust(tFac, CoH_Array, mode, numComps=12):
+    """Plots tensor factorization of cells"""
+    mode_labels = CoH_Array[mode]
+    coord = CoH_Array.dims.index(mode)
+    mode_facs = tFac[1][coord]
+    tFacDF = pd.DataFrame()
+
+    for i in range(0, numComps):
+        tFacDF = pd.concat([tFacDF, pd.DataFrame({"Component_Val": mode_facs[:, i], "Component": (i + 1), mode: mode_labels})])
+
+    tFacDF = pd.pivot(tFacDF, index="Component", columns=mode, values="Component_Val")
+    cmap = sns.color_palette("vlag", as_cmap=True)
+    status_df = pd.read_csv(join(path_here, "data/Patient_Status.csv")).sort_values(by="Patient").reset_index()
+    status = status_df["Status"]
+    lut = dict(zip(status.unique(), "rbg"))
+    col_colors = pd.DataFrame(status.map(lut))
+    col_colors["Patient"] = status_df.Patient.values
+    col_colors = col_colors.set_index("Patient")
+    f = sns.clustermap(data=tFacDF, robust=True, cmap=cmap, vmin=-1, vmax=1, row_cluster=False, col_colors=col_colors, figsize=(6, 3))
+    return f
