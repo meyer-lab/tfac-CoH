13b031a01635ee837ec6ce39c123c4c27d17c77a
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2023-01-09 15:07:30 -0800

    Fixe

--- coh/figures/figure1.py
+++ coh/figures/figure1.py
@@ -28,7 +28,7 @@ def makeFigure():
 
     marker = "pSTAT3"
     CoH_data = pd.read_csv(join(path_here, "data/CoH_Flow_DF.csv"))
-    fullHeatMap(ax[1], CoH_data, [marker], makeDF=True)
+    fullHeatMap(ax[1], CoH_data, [marker], makeDF=False)
     response_ligand_scatter(ax[2], CoH_data, marker)
     response_cell_scatter(ax[3], CoH_data, "pSTAT6", "IL4-50ng")
 
