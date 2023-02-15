2b8b82e22785dcb31a841f868ae56bae81ed3e7b
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2023-02-15 10:50:49 -0800

    Added Imputed Heatmap

--- coh/figures/figure1.py
+++ coh/figures/figure1.py
@@ -5,12 +5,14 @@ import xarray as xa
 import numpy as np
 import seaborn as sns
 import pandas as pd
+import matplotlib.pyplot as plt
 from copy import copy
+import tensorly as tl
 from tensorly.cp_tensor import cp_flip_sign
 from tensorpack.cmtf import cp_normalize, perform_CP
 from .figureCommon import subplotLabel, getSetup
 from os.path import join, dirname
-import matplotlib.pyplot as plt
+from ..tensor import factorTensor
 
 plt.rcParams['svg.fonttype'] = 'none'
 path_here = dirname(dirname(__file__))
@@ -27,7 +29,8 @@ def makeFigure():
     ax[0].axis("off")
 
     marker = "pSTAT3"
-    CoH_data = pd.read_csv(join(path_here, "data/CoH_Flow_DF.csv"))
+
+    CoH_data = make_impute_DF()
     treatments = ["IL2-50ng", "IL4-50ng", "IL6-50ng", "IL10-50ng", "IFNg-50ng", "TGFB-50ng", "IFNg-50ng+IL6-50ng", "Untreated"]
     CoH_data = CoH_data.loc[CoH_data.Treatment.isin(treatments)]
     fullHeatMap(ax[1], CoH_data, [marker], makeDF=False)
@@ -98,6 +101,7 @@ def fullHeatMap(ax, respDF, markers, makeDF=True):
                             else:
                                 row[treatment + " - " + str(time)] = entry
                 heatmapDF = pd.concat([heatmapDF, row])
+        heatmapDF["Untreated - 15 min"] = 0
         heatmapDF = heatmapDF.set_index("Patient/Cell")
         heatmapDF.to_csv(join(path_here, "data/CoH_Heatmap_DF.csv"))
     else:
@@ -125,3 +129,19 @@ def response_cell_scatter(ax, CoH_DF, marker, treatment):
     sns.boxplot(data=hist_DF, y="Mean", x="Cell", ax=ax)
     ax.set(title="Average Response to " + treatment, ylabel=marker, xlabel="Cell")
     ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha="right")
+
+
+def make_impute_DF():
+    """Imputes data and returns df containing those values"""
+    num_comps = 10
+    CoH_Data = xa.open_dataarray(join(path_here, "data/CoHTensorDataJustSignal.nc"))
+    tFacAllM, _ = factorTensor(CoH_Data.values, numComps=num_comps)
+    cp_normalize(tFacAllM)
+    tensor = tl.cp_to_tensor(tFacAllM)
+    CoH_Data_I = copy(CoH_Data)
+    CoH_Data_I.data = tensor
+    CoH_Data_DF = CoH_Data_I.to_dataframe(name=["Tensor"])
+    CoH_Data_DF= CoH_Data_DF.reset_index()
+    CoH_Data_DF.columns = CoH_Data_DF.columns.map(''.join)
+    CoH_Data_DF = CoH_Data_DF.rename(columns={"Tensor": "Mean"})
+    return CoH_Data_DF
