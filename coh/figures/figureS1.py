49fe191bcd636125c5f1019ce5036570117fc2bd
Aaron Meyer <ameyer@ucla.edu>
2023-02-27 07:15:24 -0800

    Compare factorization reduction sizes

--- coh/figures/figureS1.py
+++ coh/figures/figureS1.py
@@ -1,20 +1,15 @@
 """
 This creates Figure 1.
 """
-from os.path import join
-from .figureCommon import subplotLabel, getSetup
-from tensorpack.plot import reduction, tucker_reduction
-from tensorpack.tucker import tucker_decomp
-from tensorpack import Decomposition
+from os.path import join, dirname
 import xarray as xa
-import numpy as np
-import os
-import warnings
-warnings.filterwarnings("ignore")
 import matplotlib.pyplot as plt
+from tensorpack.plot import reduction
+from tensorpack import Decomposition
+from .figureCommon import subplotLabel, getSetup
 
-plt.rcParams['svg.fonttype'] = 'none'
-path_here = os.path.dirname(os.path.dirname(__file__))
+
+path_here = dirname(dirname(__file__))
 
 
 def makeFigure():
@@ -25,26 +20,12 @@ def makeFigure():
     # Add subplot labels
     subplotLabel(ax)
 
-    CoH_Data = xa.open_dataarray(join(path_here, "data/CoHTensorDataJustSignal.nc"))
+    X = xa.open_dataarray(join(path_here, "data/CoHTensorDataJustSignal.nc")).to_numpy()
 
-    # makePCA_df(CoH_Data)
-    # plot_PCA(ax[0:2])
-
-    # perform parafac
-    decomp_data = CoH_Data.to_numpy()
-    nan_index = []
-    for i in range(0, decomp_data.shape[0]):
-        nan_index.append(~np.isnan(decomp_data[i, :, :, :]).any())
-    decomp_data = decomp_data[nan_index, :, :, :]
-
-    tc = Decomposition(decomp_data, max_rr=8)
+    tc = Decomposition(X, max_rr=8)
     tc.perform_tfac()
     tc.perform_PCA(flattenon=2)
 
     reduction(ax[0], tc)
-    tuck = Decomposition(decomp_data, method=tucker_decomp, max_rr=8)
-    para = Decomposition(decomp_data, max_rr=8)
-    tucker_reduction(ax[1], tuck, para)
-    # plot_PCA(ax[0:2])
 
     return f
