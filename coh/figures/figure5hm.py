b004c10214600a430561aad6b546afc952a1fcdd
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2023-03-02 17:43:09 -0800

    Switch to Partial Correlations

--- coh/figures/figure5hm.py
+++ coh/figures/figure5hm.py
@@ -3,6 +3,7 @@ This creates Figure 5 heatmap (clustered receptor and response correlations).
 """
 import seaborn as sns
 import pandas as pd
+import numpy as np
 from .figureCommon import subplotLabel, getSetup
 from os.path import join, dirname
 
@@ -25,10 +26,9 @@ def makeFigure():
     CoH_Data_DF_R = pd.read_csv(join(path_here, "data/CoH_Rec_DF.csv"))
 
     # CD8
-    # f = dysreg_cor_hm_R(CoH_DF, CoH_DF_B, CoH_Data_DF_R, ["pSTAT3", "pSTAT5", "pSmad1-2"], ["IL10-50ng", "IL2-50ng", "TGFB-50ng"], ["pSmad1-2", "pSTAT4"], ["TGFB RII", "PD_L1", "IL6Ra", "IL10R", "IL12RI", "IL2RB"], cells=["CD8+"])
+    f = dysreg_cor_hm_R(CoH_DF, CoH_DF_B, CoH_Data_DF_R, ["pSTAT3", "pSTAT5"], ["IL10-50ng", "IL2-50ng"], ["pSmad1-2", "pSTAT4"], ["TGFB RII", "PD_L1", "IL6Ra", "IL10R", "IL12RI", "IL2RB"], cells=["CD8+"])
     # CD4
-    f = dysreg_cor_hm_R(CoH_DF, CoH_DF_B, CoH_Data_DF_R, ["pSTAT3", "pSmad1-2"], ["IL10-50ng", "TGFB-50ng"],
-                        ["pSmad1-2", "pSTAT4", "pSTAT1"], ["TGFB RII", "IL10R", "IL6Ra", "IL12RI", "IFNg R1"], cells=["CD4+"])
+    # f = dysreg_cor_hm_R(CoH_DF, CoH_DF_B, CoH_Data_DF_R, ["pSTAT3"], ["IL10-50ng"], ["pSmad1-2", "pSTAT4", "pSTAT1"], ["TGFB RII", "IL10R", "IL6Ra", "IL12RI", "IFNg R1"], cells=["CD4+"])
     # Bcell
     # f = dysreg_cor_hm_R(CoH_DF, CoH_DF_B, CoH_Data_DF_R, ["pSTAT3", "pSTAT5", "pSmad1-2"], ["IL10-50ng", "IL2-50ng", "TGFB-50ng"], ["pSmad1-2", "pSTAT4"], ["TGFB RII", "PD_L1", "IL6Ra", "IL10R", "IL12RI", "IL2RB", "IL2Ra"], cells=["CD20 B"])
     # Treg
@@ -78,7 +78,17 @@ def dysreg_cor_hm_R(CoH_DF, CoH_DF_B, CoH_DF_R, markers_dysreg, cyto_dysreg, mar
             value = patient_DF_R.loc[(patient_DF_R.Marker == marker)].Mean.values
             patient_row[marker] = value
         dysreg_DF = pd.concat([dysreg_DF, patient_row])
+    
+    print(dysreg_DF)
+    
+    cov_DF = dysreg_DF.cov()
+    Vi = np.linalg.pinv(cov_DF, hermitian=True)  # Inverse covariance matrix
+    Vi_diag = Vi.diagonal()
+    D = np.diag(np.sqrt(1 / Vi_diag))
+    pCor = -1 * (D @ Vi @ D)  # Partial correlation matrix
+    pCor[np.diag_indices_from(pCor)] = 1
+    pCorr_DF = pd.DataFrame(pCor, columns=cov_DF.columns, index=cov_DF.columns)
 
     cmap = sns.color_palette("vlag", as_cmap=True)
-    f = sns.clustermap(data=dysreg_DF.corr(), robust=True, vmin=-1, vmax=1, row_cluster=True, col_cluster=True, annot=True, cmap=cmap, figsize=(8, 8))
+    f = sns.clustermap(pCorr_DF, robust=True, vmin=-1, vmax=1, row_cluster=True, col_cluster=True, annot=True, cmap=cmap, figsize=(8, 8))
     return f
