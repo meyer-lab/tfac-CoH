59b73bf7531ef5f36c818a4110c07546c4bf2b6e
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2023-03-21 12:40:19 -0700

    Fixup

--- coh/figures/figureS1.py
+++ coh/figures/figureS1.py
@@ -14,7 +14,7 @@ path_here = dirname(dirname(__file__))
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
     # Get list of axis objects
-    ax, f = getSetup((4.5, 3), (1, 1))
+    ax, f = getSetup((3, 3), (1, 1))
 
     # Add subplot labels
     subplotLabel(ax)
