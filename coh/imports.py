394e0b143f47bdc9a4d0cd230b279e5164762e66
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2023-04-13 15:13:46 -0700

    Small Fix

--- coh/imports.py
+++ coh/imports.py
@@ -17,7 +17,7 @@ path_here = os.path.dirname(os.path.dirname(__file__))
 
 def importCITE():
     """Downloads all surface markers and cell types"""
-    CITEmarkerDF = pd.read_csv(join(path_here, "opt/CoH/SingleCell/CITEdata_SurfMarkers.zip"))
+    CITEmarkerDF = pd.read_csv(join(path_here, "/opt/CoH/SingleCell/CITEdata_SurfMarkers.zip"))
     return CITEmarkerDF
 
 
@@ -52,17 +52,17 @@ def makeRNAseqDF(surface=False):
     matrix2 = coo_matrix((matrix.data[dataCoords], (matrix.col[dataCoords], vec_translate(matrix.row[dataCoords], colDict))), shape=(matrix.shape[1], np.count_nonzero(featInd)))
     geneCols = allFeatures[featInd]
     GeneDF = pd.DataFrame(data=matrix2.toarray(), columns=geneCols)
-    cellTypesDF = pd.read_csv(join(path_here, "opt/CoH/SingleCell/CITEcellTypes.csv"))
+    cellTypesDF = pd.read_csv(join(path_here, "/opt/CoH/SingleCell/CITEcellTypes.csv"))
     GeneDF = pd.concat([GeneDF, cellTypesDF], axis=1)
     if surface:
-        GeneDF.to_csv(join(path_here, "opt/CoH/SingleCell/RNAseqSurface.csv.zip"))
+        GeneDF.to_csv(join(path_here, "/opt/CoH/SingleCell/RNAseqSurface.csv.zip"))
     else:
-        GeneDF.to_csv(join(path_here, "opt/CoH/SingleCell/RNAseq.csv.zip"))
+        GeneDF.to_csv(join(path_here, "/opt/CoH/SingleCell/RNAseq.csv.zip"))
 
 
 def makeRNAseqDF_Ann(surface=False):
     """Read data and make AnnData object"""
-    RNA = ad.read_h5ad("opt/CoH/SingleCell/CITE_RNA_raw.h5ad").transpose()
+    RNA = ad.read_h5ad("/opt/CoH/SingleCell/CITE_RNA_raw.h5ad").transpose()
     cellTypesDF = pd.read_csv(join(path_here, "coh/data/SC_seq/CITEcellTypes.csv"))
     featuresGenes = pd.read_csv("coh/data/SC_seq/RNAfeatures.csv")
     surfGenes = pd.read_csv("coh/data/SC_seq/SurfaceGenes.csv")
@@ -75,15 +75,15 @@ def makeRNAseqDF_Ann(surface=False):
     RNA.var_names_make_unique()
     if surface:
         RNA = RNA[:, np.isin(RNA.var.values, surfGenes["Gene"].values)]
-        RNA.write_h5ad("opt/CoH/SingleCell/CITE_RNA_Surface.h5ad.gz", compression='gzip')
+        RNA.write_h5ad("/opt/CoH/SingleCell/CITE_RNA_Surface.h5ad.gz", compression='gzip')
     else:
-        RNA.write_h5ad("opt/CoH/SingleCell/CITE_RNA.h5ad.gz", compression='gzip')
+        RNA.write_h5ad("/opt/CoH/SingleCell/CITE_RNA.h5ad.gz", compression='gzip')
 
 
 def importRNACITE(surface=False):
     """Downloads all surface markers and cell types"""
     if surface:
-        RNA_DF = ad.read_h5ad("opt/CoH/SingleCell/CITE_RNA_Surface.h5ad.gz")
+        RNA_DF = ad.read_h5ad("/opt/CoH/SingleCell/CITE_RNA_Surface.h5ad.gz")
     else:
-        RNA_DF = ad.read_h5ad("opt/CoH/SingleCell/CITE_RNA.h5ad.gz")
+        RNA_DF = ad.read_h5ad("/opt/CoH/SingleCell/CITE_RNA.h5ad.gz")
     return RNA_DF
