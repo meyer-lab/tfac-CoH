71c45ba8b739f6c386ba6bdbab947482f9bf33e8
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2023-08-06 15:55:24 -0700

    Figure 3 plots added

--- coh/figures/figure7.py
+++ coh/figures/figure7.py
@@ -1,18 +1,19 @@
 """
-This creates Figure S2, factorization of fold-change data.
+This creates Figure 3, dissection of signaling.
 """
+import pickle
 import pandas as pd
 import numpy as np
 import seaborn as sns
 from scipy.stats import zscore
 from .common import subplotLabel, getSetup, BC_scatter_cells
-from ..flow_rec import get_status_rec_df
+from ..flow import make_CoH_Tensor, get_status_df
 
 
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
     # Get list of axis objects
-    ax, f = getSetup((12, 7), (2, 4))
+    ax, f = getSetup((12, 10), (3, 4))
 
     # Add subplot labels
     subplotLabel(ax)
@@ -21,6 +22,15 @@ def makeFigure():
     CoH_DF = CoH_DF.loc[CoH_DF.Treatment.isin(treatments)].dropna()
 
     # Figure A Markers for signaling component
+
+    CoH_Data = make_CoH_Tensor(just_signal=True)
+
+    with open('./coh/data/signaling.pkl', 'rb') as ff:
+        tFacAllM = pickle.load(ff) # 12 component
+    mode = CoH_Data.dims[1]
+    tFacDF = pd.DataFrame(tFacAllM.factors[1], index=CoH_Data.coords[mode], columns=[str(i + 1) for i in range(tFacAllM.factors[1].shape[1])])
+    cmap = sns.color_palette("vlag", as_cmap=True)
+    sns.heatmap(data=tFacDF, ax=ax[0], cmap=cmap, vmin=-1, vmax=1, cbar=(True))
 
     # B Baseline pSTAT3
 
@@ -31,9 +41,7 @@ def makeFigure():
     meanDF = CoH_DF.groupby(["Patient", "Cell", "Treatment", "Marker"]).mean().reset_index()
 
     meanDF = (meanDF.pivot(index=["Patient", "Cell", "Treatment"], columns="Marker", values="Mean").reset_index().set_index("Patient"))
-    print(meanDF)
     meanDF.iloc[:, 2::] = meanDF.iloc[:, 2::].apply(zscore)
-    print(meanDF)
 
     plot_by_patient(
         meanDF,
@@ -46,7 +54,7 @@ def makeFigure():
         ax=ax[2],
     )
 
-    # C CD8+ pSTAT3 vs B pSTAT3 in IL10
+    # D CD8+ pSTAT3 vs B pSTAT3 in IL10
 
     plot_by_patient(
         meanDF,
@@ -58,24 +66,127 @@ def makeFigure():
         treatment2="IL10-50ng",
         ax=ax[3],
     )
+    
+    # E CD8+ pSTAT3 vs B pSTAT3 in IL10
+
+    plot_by_patient(
+        meanDF,
+        cell1="CD8+",
+        receptor1="pSTAT5",
+        treatment1="Untreated",
+        cell2="CD8+",
+        receptor2="pSTAT5",
+        treatment2="IL2-50ng",
+        ax=ax[4],
+    )
+    
+
+    # F CD8+ pSTAT5 vs Treg pSTAT5 in IL2
+
+    plot_by_patient(
+        meanDF,
+        cell1="CD8+",
+        receptor1="pSTAT5",
+        treatment1="IL2-50ng",
+        cell2="Treg",
+        receptor2="pSTAT5",
+        treatment2="IL2-50ng",
+        ax=ax[5],
+    )
+
+    # G CD8+ pSTAT3 vs B pSTAT3 in IL10
+
+    plot_by_patient(
+        meanDF,
+        cell1="CD8+",
+        receptor1="pSTAT5",
+        treatment1="IL2-50ng",
+        cell2="Treg",
+        receptor2="pSTAT5",
+        treatment2="IL2-50ng",
+        ax=ax[6],
+    )
+
+    # H Untreated pSTAT4 and pSMAD1-2 by cell
+
+    meanDF_cell = CoH_DF.groupby(["Cell", "Treatment", "Marker"]).mean().reset_index()
+
+    meanDF_cell = (meanDF_cell.pivot(index=["Cell", "Treatment"], columns="Marker", values="Mean").reset_index().set_index("Cell"))
+    meanDF_cell.iloc[:, 2::] = meanDF_cell.iloc[:, 2::].apply(zscore)
+
+    plot_by_cell(
+        meanDF_cell,
+        receptor1="pSTAT4",
+        treatment1="Untreated",
+        receptor2="pSmad1-2",
+        treatment2="Untreated",
+        ax=ax[7],
+    )
+
+    # I pSTAT4 vs pSMAD untreated per patient
+
+    meanDF_pat = CoH_DF.groupby(["Patient", "Treatment", "Marker"]).mean().reset_index()
+
+    meanDF_pat = (meanDF_pat.pivot(index=["Patient", "Treatment"], columns="Marker", values="Mean").reset_index().set_index("Patient"))
+    meanDF_pat.iloc[:, 2::] = meanDF_pat.iloc[:, 2::].apply(zscore)
+
+    plot_per_patient(
+        meanDF_pat,
+        receptor1="pSTAT4",
+        treatment1="Untreated",
+        receptor2="pSmad1-2",
+        treatment2="Untreated",
+        ax=ax[8],
+    )
 
     return f
 
 
 def plot_by_patient(sigDF, cell1, receptor1, treatment1, cell2, receptor2, treatment2, ax):
     """Plots receptor in pop 1 vs receptor in pop 2 per patient, by disease status"""
-    status_DF = get_status_rec_df()
+    status_DF = get_status_df()
     plotDF = pd.DataFrame({"Patient": sigDF.loc[(sigDF.Cell == cell1) & (sigDF.Treatment == treatment1)].index.values})
     plotDF[cell1 + " " + receptor1 + " " + treatment1] = sigDF.loc[(sigDF.Cell == cell1) & (sigDF.Treatment == treatment1)][receptor1].values
     plotDF[cell2 + " " + receptor2 + " " + treatment2] = sigDF.loc[(sigDF.Cell == cell2) & (sigDF.Treatment == treatment2)][receptor2].values
     plotDF = plotDF.set_index("Patient").join(
         status_DF.set_index("Patient"), on="Patient"
     )
-    print(plotDF.corr())
     sns.scatterplot(
         data=plotDF,
         x=cell1 + " " + receptor1 + " " + treatment1,
         y=cell2 + " " + receptor2 + " " + treatment2,
+        ax=ax,
+        hue="Status",
+    )
+
+
+def plot_by_cell(sigDF, receptor1, treatment1, receptor2, treatment2, ax):
+    """Plots receptor in pop 1 vs receptor in pop 2 per patient, by disease status"""
+    plotDF = pd.DataFrame({"Cell": sigDF.loc[(sigDF.Treatment == treatment1)].index.values})
+    plotDF[receptor1 + " " + treatment1] = sigDF.loc[(sigDF.Treatment == treatment1)][receptor1].values
+    plotDF[receptor2 + " " + treatment2] = sigDF.loc[(sigDF.Treatment == treatment2)][receptor2].values
+    sns.scatterplot(
+        data=plotDF,
+        x=receptor1 + " " + treatment1,
+        y=receptor2 + " " + treatment2,
+        ax=ax,
+        hue="Cell",
+    )
+
+
+def plot_per_patient(sigDF, receptor1, treatment1, receptor2, treatment2, ax):
+    """Plots receptor in pop 1 vs receptor in pop 2 per patient, by disease status"""
+    status_DF = get_status_df()
+    plotDF = pd.DataFrame({"Patient": sigDF.loc[(sigDF.Treatment == treatment1)].index.values})
+    plotDF[receptor1 + " " + treatment1] = sigDF.loc[(sigDF.Treatment == treatment1)][receptor1].values
+    plotDF[receptor2 + " " + treatment2] = sigDF.loc[(sigDF.Treatment == treatment2)][receptor2].values
+    plotDF = plotDF.set_index("Patient").join(
+        status_DF.set_index("Patient"), on="Patient"
+    )
+    sns.scatterplot(
+        data=plotDF,
+        x=receptor1 + " " + treatment1,
+        y=receptor2 + " " + treatment2,
         ax=ax,
         hue="Status",
     )
