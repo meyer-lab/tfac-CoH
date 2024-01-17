7d63661ec62120b0f36b395dfd418c58f87bc8dd
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2024-01-16 16:35:58 -0800

    More 2 Changes

--- coh/figures/figure2.py
+++ coh/figures/figure2.py
@@ -33,26 +33,30 @@ def makeFigure():
     with open('./coh/data/signaling.pkl', 'rb') as ff:
         tFacAllM = pickle.load(ff) # 12 component
 
-    BC_status_plot(13, CoH_Data, ax[5], get_status_df())
-    CoH_LogReg_plot(ax[6], tFacAllM, CoH_Data, get_status_df())
+    #BC_status_plot(13, CoH_Data, ax[5], get_status_df())
+    ##CoH_LogReg_plot(ax[6], tFacAllM, CoH_Data, get_status_df())
     
     # B cells and Tregs in response to different stimulations
     CoH_DF = pd.read_csv("./coh/data/CoH_Flow_DF.csv")
     CoH_DF = CoH_DF.loc[CoH_DF.Time == "15min"]
     cytok_stim_plot(CoH_DF, "IL4-50ng", ["CD20 B", "Treg"], ax=ax[6])
-    ax[6].set(ylim=(-200, 1000))
+    ax[6].set(ylim=(-750, 1500))
 
     # IFNg by status
     CoH_DF = pd.read_csv("./coh/data/CoH_Flow_DF.csv")
     CoH_DF = CoH_DF.loc[CoH_DF.Time == "15min"]
-    ligs = ["IFNg-50ng", "IL10-50ng"]
+    ligs = ["IL2-50ng", "IL10-50ng"]
     axes = [7, 9]
+    cells = ["CD16 NK", "CD20 B", "CD4+", "CD8+", "CD33 Myeloid", "Treg"]
 
-    for i, sigg in enumerate(["pSTAT1", "pSTAT3"]):
+    for i, sigg in enumerate(["pSTAT5", "pSTAT3"]):
         DF = CoH_DF.loc[(CoH_DF.Marker == sigg) & (CoH_DF.Treatment == ligs[i])]
         DF["Mean"] -= np.nanmean(DF["Mean"].values)
         DF["Mean"] /= np.nanstd(DF["Mean"].values)
+        DF = DF[(CoH_DF.Cell.isin(cells))]
         BC_scatter_cells(ax[axes[i]], DF, sigg, ligs[i])
+    ax[7].set(ylim=(-3, 4))
+    ax[9].set(ylim=(-2, 4))
 
     # Correlation plot
     comp_corr_plot(tFacAllM, CoH_Data, get_status_df(), ax[8])
@@ -65,7 +69,7 @@ def makeFigure():
 def cytok_stim_plot(CoH_DF, cytok, cells, ax):
     """Plots cells responses across signaling products for a single stimulatiom"""
     CoH_DF = CoH_DF.loc[(CoH_DF.Treatment == cytok) & (CoH_DF.Cell.isin(cells)) & CoH_DF.Marker.isin(["pSTAT1", "pSTAT3", "pSTAT3", "pSTAT5", "pSTAT6", "pSmad1-2"])]
-    sns.barplot(data=CoH_DF, x="Cell", y="Mean", hue="Marker", palette='husl', ax=ax)
+    sns.boxplot(data=CoH_DF, x="Cell", y="Mean", hue="Marker", palette='husl', showfliers=False, ax=ax)
     ax.set(ylabel="Response to " + cytok)
 
 
