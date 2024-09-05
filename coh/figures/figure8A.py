87eedcf37c44ea37b504e2277c52d3d0529f5824
Cyrillus <cyztan@gmail.com>
2024-09-05 01:10:58 -0600

    Add Tucker results for both figures

--- coh/figures/figure8A.py
+++ coh/figures/figure8A.py
@@ -3,33 +3,17 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from sklearn import preprocessing
-from tensorpack.decomposition import Decomposition
-from tensorpack.plot import tucker_reduction
-from tensorpack.tucker import tucker_decomp
 from tensorly.decomposition import tucker
 from itertools import product
 
-from ..flow import get_status_df, make_CoH_Tensor
-from ..flow_rec import get_status_rec_df, make_CoH_Tensor_rec
+from ..tensor import factorTensor
+from ..flow import make_CoH_Tensor
 from .common import getSetup, subplotLabel
 
 
-def makeFigure():
-    """ Tucker decomposition figure """
-    ax, f = getSetup((12, 8), (2, 3))
-    subplotLabel(ax)
-
-    #CoH_Data = make_CoH_Tensor(just_signal=True)
-    CoH_Data = make_CoH_Tensor_rec()
-    data = CoH_Data.to_numpy()
+def explore_tucker_rank(data):
+    """ Find all optimal Tucker ranks through Pareto improvement  """
     mask = np.isfinite(data)
-
-    tres = tucker(np.nan_to_num(data), rank=(8, 6, 4, 6), mask=mask, svd='randomized_svd')
-    R2X = 1 - np.nansum((data - tres.to_tensor()) ** 2) / np.nansum(data ** 2)
-    # CP 12 comp = 0.8058984095216497
-
-    # Tucker rank explore
     start_rank = [1] * data.ndim
     tucker_rank = [start_rank]
     ttry = tucker(np.nan_to_num(data), rank=start_rank, mask=mask, svd='randomized_svd')
@@ -49,14 +33,14 @@ def makeFigure():
         tucker_rank.append(try_ranks[np.argmax(try_R2X)])
         tucker_R2X.append(np.max(try_R2X))
         start_rank = try_ranks[np.argmax(try_R2X)]
-    tucker_ranks = pd.DataFrame({
+    return pd.DataFrame({
         "Rank": tucker_rank,
         "R2X": tucker_R2X,
         "Method": ["Tucker"] * len(tucker_R2X)
     })
 
 
-    # visualize core tensor
+def visualize_tucker(ax, tres, xr_data):
     core_tensor = pd.DataFrame({
         "Index": product(*[np.arange(1, si + 1) for si in tres[0].shape]),
         "Weight": tres[0].flatten(),
@@ -74,20 +58,40 @@ def makeFigure():
     # visualize factors
     factors = [pd.DataFrame(tres[1][rr],
                             columns=[f"Cmp. {i}" for i in np.arange(1, tres[1][rr].shape[1] + 1)],
-                            index=CoH_Data.coords[CoH_Data.coords.dims[rr]].to_numpy())
-               for rr in range(data.ndim)]
+                            index=xr_data.coords[xr_data.coords.dims[rr]].to_numpy())
+               for rr in range(xr_data.ndim)]
 
-    for rr in range(data.ndim):
-        if CoH_Data.coords.dims[rr] == "Patient":
-            sns.clustermap(factors[rr], col_cluster=False, method='average',
-                           cmap="vlag", vmin=-1.0, vmax=1.0, cbar=True, ax=ax[rr + 1])
-        else:
-            sns.heatmap(factors[rr], cmap="vlag", center=0,
-                        xticklabels=[str(ii + 1) for ii in range(tres[1][rr].shape[1])],
-                        yticklabels=factors[rr].index,
-                        cbar=True, vmin=-1.0, vmax=1.0, ax=ax[rr + 1])
+    for rr in range(xr_data.ndim):
+        sns.heatmap(factors[rr], cmap="vlag", center=0,
+                    xticklabels=[str(ii + 1) for ii in range(tres[1][rr].shape[1])],
+                    yticklabels=factors[rr].index,
+                    cbar=True, vmin=-1.0, vmax=1.0, ax=ax[rr + 1])
         ax[rr + 1].set_xlabel("Components")
-        ax[rr + 1].set_title(CoH_Data.coords.dims[rr])
+        ax[rr + 1].set_title(xr_data.coords.dims[rr])
+    return ax
+
+
+def makeFigure():
+    """ Tucker decomposition figure """
+    ax, f = getSetup((12, 8), (2, 3))
+    subplotLabel(ax)
+
+    CoH_Data = make_CoH_Tensor(just_signal=True)
+    data = CoH_Data.to_numpy()
+    mask = np.isfinite(data)
+
+    tres = tucker(np.nan_to_num(data), rank=(8, 6, 4, 6), mask=mask, svd='randomized_svd')
+    R2X = 1 - np.nansum((data - tres.to_tensor()) ** 2) / np.nansum(data ** 2)
+    print(f"Tucker R2X is {R2X}")
+    # CP 12 comp = 0.805370012138839
+
+    # visualize core tensor
+    visualize_tucker(ax, tres, CoH_Data)
 
     return f
-    f.savefig("new_fig8.pdf")
+    f.savefig("new_fig8a.pdf")
+
+def check_CP_R2X(data):
+    """ Document code used to check equivalent CP R2X levels """
+    for i in range(1, 13):
+        print(i, factorTensor(data, i).R2X)
